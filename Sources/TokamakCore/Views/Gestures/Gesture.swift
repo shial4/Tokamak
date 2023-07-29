f7c39affdd373f5ff0ffe1095d9948dd7f2c95ba
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2023-07-29 20:14:21 +1000

    .

--- Sources/TokamakCore/Views/Gestures/Gesture.swift
+++ Sources/TokamakCore/Views/Gestures/Gesture.swift
@@ -48,12 +48,12 @@ extension Gesture {
 
 extension Gesture {
     /// Adds an action to perform when the gesture ends.
-    func onEnded(_ action: @escaping (Value) -> Void) -> _EndedGesture<Self> {
+    public func onEnded(_ action: @escaping (Value) -> Void) -> _EndedGesture<Self> {
         _EndedGesture(gesture: self, action: action)
     }
     
     /// Updates the provided gesture value property as the gesture’s value changes.
-    func updating<State>(_ value: GestureState<State>, body: @escaping (Value, inout State, inout Transaction) -> Void) -> GestureStateGesture<Self, State> {
+    public func updating<State>(_ value: GestureState<State>, body: @escaping (Value, inout State, inout Transaction) -> Void) -> GestureStateGesture<Self, State> {
         GestureStateGesture(gesture: self, gestureState: value)
     }
 }
@@ -61,7 +61,7 @@ extension Gesture {
 extension Gesture where Value: Equatable {
     /// Adds an action to perform when the gesture’s value changes.
     /// Available when Value conforms to Equatable.
-    func onChanged(_ action: @escaping (Value) -> Void) -> _ChangedGesture<Self> {
+    public func onChanged(_ action: @escaping (Value) -> Void) -> _ChangedGesture<Self> {
         _ChangedGesture(gesture: self, action: action)
     }
 }
@@ -70,17 +70,17 @@ extension Gesture where Value: Equatable {
 
 extension Gesture {
     /// Combines a gesture with another gesture to create a new gesture that recognizes both gestures at the same time.
-    func simultaneously<Other>(with gesture: Other) -> SimultaneousGesture<Self, Other> {
+    public func simultaneously<Other>(with gesture: Other) -> SimultaneousGesture<Self, Other> {
         SimultaneousGesture(first: self, second: gesture)
     }
     
     /// Sequences a gesture with another one to create a new gesture, which results in the second gesture only receiving events after the first gesture succeeds.
-    func sequenced<Other>(before gesture: Other) -> SequenceGesture<Self, Other> {
+    public func sequenced<Other>(before gesture: Other) -> SequenceGesture<Self, Other> {
         SequenceGesture(first: self, second: gesture)
     }
     
     /// Combines two gestures exclusively to create a new gesture where only one gesture succeeds, giving precedence to the first gesture.
-    func exclusively<Other>(before gesture: Other) -> ExclusiveGesture<Self, Other> {
+    public func exclusively<Other>(before gesture: Other) -> ExclusiveGesture<Self, Other> {
         ExclusiveGesture(first: self, second: gesture)
     }
 }
