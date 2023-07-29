c89425cad03e8c578acec6edfb27127600f74df6
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2023-07-29 23:18:36 +1000

    l

--- Sources/TokamakCore/Views/Gestures/GestureState.swift
+++ Sources/TokamakCore/Views/Gestures/GestureState.swift
@@ -18,40 +18,30 @@
 
 @propertyWrapper
 public struct GestureState<Value>: DynamicProperty {
-  private let initialValue: Value
-
-  var anyInitialValue: Any { initialValue }
-
-  var getter: (() -> Any)?
-  var setter: ((Any, Transaction) -> ())?
-
-  public init(wrappedValue value: Value) {
-    initialValue = value
-  }
-
-  public var wrappedValue: Value {
-    get { getter?() as? Value ?? initialValue }
-    nonmutating set { setter?(newValue, Transaction._active ?? .init(animation: nil)) }
-  }
-
-  public var projectedValue: Binding<Value> {
-    guard let getter = getter, let setter = setter else {
-      fatalError("\(#function) not available outside of `body`")
+    private let initialValue: Value
+    
+    var anyInitialValue: Any { initialValue }
+    
+    var getter: (() -> Any)?
+    var setter: ((Any, Transaction) -> ())?
+    
+    public init(wrappedValue value: Value) {
+        initialValue = value
     }
-    // swiftlint:disable force_cast
-    return .init(
-      get: { getter() as! Value },
-      set: { newValue, transaction in
-        setter(newValue, Transaction._active ?? transaction)
-      }
-    )
-    // swiftlint:enable force_cast
-  }
+    
+    public var wrappedValue: Value {
+        get { getter?() as? Value ?? initialValue }
+        nonmutating set { setter?(newValue, Transaction._active ?? .init(animation: nil)) }
+    }
+    
+    public var projectedValue: GestureState<Value> {
+        self
+    }
 }
 
 extension GestureState: WritableValueStorage {}
 
 public extension GestureState where Value: ExpressibleByNilLiteral {
-  @inlinable
-  init() { self.init(wrappedValue: nil) }
+    @inlinable
+    init() { self.init(wrappedValue: nil) }
 }
